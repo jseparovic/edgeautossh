994693a9f4c83eefe392e8a246ecec2bf2cf9e54
Jason Separovic <jason.separovic@nokia.com>
2022-07-20 18:38:25 -0700

    update

--- files/usr/sbin/autosshwrap.sh
+++ files/usr/sbin/autosshwrap.sh
@@ -29,6 +29,7 @@ main() {
   # Start autossh in the foreground so it restarts by supervisord on failure
   /usr/bin/autossh -M ${AUTO_SSH_MONITORING_PORT} -N -R ${port}:127.0.0.1:22 \
     -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i /etc/autossh/id_rsa \
+    -o ExitOnForwardFailure=yes -o ServerAliveInterval=5 -o ServerAliveCountMax=3 \
     -p ${AUTO_SSH_WRAP_JUMP_PORT} ${AUTO_SSH_WRAP_JUMP_USER}@${AUTO_SSH_WRAP_JUMP_SERVER}
 }
 
