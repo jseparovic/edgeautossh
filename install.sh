46df712231c2c2eaac5a909b5615da370e7435d3
Jason Separovic <jason.separovic@nokia.com>
2022-07-20 17:56:05 -0700

    use updated supervisord from github

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/bash -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
 
 yum install epel-release -y
-yum -y install supervisor autossh dmidecode
+yum -y install autossh dmidecode
+
+easy_install-3.6 pip
+pip3 install git+https://github.com/jseparovic/supervisor.git@maxbackoff
 
 cp -rf files/* /
 
